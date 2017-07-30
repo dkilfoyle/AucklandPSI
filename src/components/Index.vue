8a8691ef1a7c03e2d82140489e86b2ed8c498e1d
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-07-30 22:30:34 +1200

    use stylus iteration for png icons

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
   q-layout(ref="layout" view="lHh LpR lFf")
     q-toolbar(slot="header")
+      q-btn(flat @click="$refs.layout.toggleLeft()")
+        q-icon(name="menu")
       q-icon(name="brain")
-      q-toolbar-title
-        |Northern/Midlands Percutaneous Stroke Intervention Screening Tool
+      q-toolbar-title Auckland PSI
+        span(slot="subtitle") Northern/Midlands Percutaneous Stroke Intervention Screening Tool
       q-chip(color="secondary" icon="build") beta
         q-tooltip Development version. Send any comments to Dean Kilfoyle
 
@@ -136,21 +138,44 @@ export default {
     background-repeat: no-repeat;
 }
 
-.png-keyboard_arrow_right { background-image: url('~assets/keyboard_arrow_right.png'); }
-.png-keyboard_arrow_left { background-image: url('~assets/keyboard_arrow_left.png'); }
-.png-scanner { background-image: url('~assets/scannerw.png'); }
-.png-access_time { background-image: url('~assets/access_timew.png'); }
-.png-flight { background-image: url('~assets/flightw.png'); }
-.png-build { background-image: url('~assets/buildw.png'); }
-.png-cached { background-image: url('~assets/cached.png'); }
-.png-person { background-image: url('~assets/personw.png'); }
-.png-arrow_drop_down { background-image: url('~assets/arrow_drop_down.png'); }
-.png-check_box_outline_blank { background-image: url('~assets/check_box_outline_blank.png'); }
-.png-check_box { background-image: url('~assets/check_box.png'); }
-.png-radio_button_unchecked { background-image: url('~assets/radio_button_unchecked.png'); }
-.png-radio_button_checked { background-image: url('~assets/radio_button_checked.png'); }
-.png-check { background-image: url('~assets/checkw.png'); }
-.png-pass { background-image: url('~assets/pass.png'); }
-.png-cancel { background-image: url('~assets/cancel.png'); }
-.png-brain { background-image: url('~assets/brain.png'); }
+icons = keyboard_arrow_right,
+ keyboard_arrow_left,
+ scanner,
+ access_time,
+ flight,
+ build,
+ cached,
+ person,
+ arrow_drop_down,
+ check_box_outline_blank,
+ check_box,
+ radio_button_unchecked,
+ check,
+ pass,
+ cancel,
+ brain,
+ menu
+
+for iconname in icons
+  .png-{iconname}
+    background-image: url('~assets/' + iconname + '.png')
+
+// .png-keyboard_arrow_right { background-image: url('~assets/keyboard_arrow_right.png'); }
+// .png-keyboard_arrow_left { background-image: url('~assets/keyboard_arrow_left.png'); }
+// .png-scanner { background-image: url('~assets/scannerw.png'); }
+// .png-access_time { background-image: url('~assets/access_timew.png'); }
+// .png-flight { background-image: url('~assets/flightw.png'); }
+// .png-build { background-image: url('~assets/buildw.png'); }
+// .png-cached { background-image: url('~assets/cached.png'); }
+// .png-person { background-image: url('~assets/personw.png'); }
+// .png-arrow_drop_down { background-image: url('~assets/arrow_drop_down.png'); }
+// .png-check_box_outline_blank { background-image: url('~assets/check_box_outline_blank.png'); }
+// .png-check_box { background-image: url('~assets/check_box.png'); }
+// .png-radio_button_unchecked { background-image: url('~assets/radio_button_unchecked.png'); }
+// .png-radio_button_checked { background-image: url('~assets/radio_button_checked.png'); }
+// .png-check { background-image: url('~assets/checkw.png'); }
+// .png-pass { background-image: url('~assets/pass.png'); }
+// .png-cancel { background-image: url('~assets/cancel.png'); }
+// .png-brain { background-image: url('~assets/brain.png'); }
+// .png-menu { background-image: url('~assets/menu.png'); }
 </style>
