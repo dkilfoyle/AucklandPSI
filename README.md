1fe75725a1c3f4cf4c25bc9708934fb323885c80
dkilfoyle <dkilfoyle@xtra.co.nz>
2018-02-24 08:34:08 +1300

    restart with a blank 0.15 starter template

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
-# Auckland PSI
+# Quasar App
 
-> An online decision tool for selecting patients for Percutaneous Stroke Intervention
-
-## Instructions for Local Installation
-
-1. Install Node.js
-
-2. Install quasar-cli
-
-  ``` bash
-  # global install quasar-cli
-  $ npm install -g quasar-cli
-  ```
-
-3. Fork or download this repository to local directory
-
-4. cd to local directory and run
-
-  ``` bash
-  # install dependencies
-  $ npm install
-
-  # serve with hot reload at localhost:8080
-  $ quasar dev
-
-  # build for production with minification
-  $ quasar build
-  ```
+> WIP
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import patientCriteria from './patientCriteria.js'
-import scanCriteria from './scanCriteria.js'
-import onsetCriteria from './onsetCriteria.js'
+import example from './module-example'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   modules: {
-    patientCriteria,
-    scanCriteria,
-    onsetCriteria
-  },
-  state: {
-    dhbhospital: 'waikato'
-  },
-  getters: {
-    minsRemaining: (state, getters) => {
-      if (state.scanCriteria.ctfinding === 'MCA') { return (6 * 60 - getters.minsSinceOnset) }
-      if (state.scanCriteria.ctfinding === 'ICA') { return (6 * 60 - getters.minsSinceOnset) }
-      if (state.scanCriteria.ctfinding === 'basilar') { return (12 * 60 - getters.minsSinceOnset) }
-    }
-  },
-  mutations: {
-    setDHBHospital (state, value) { state.dhbhospital = value }
-  },
-  actions: {
-    resetCriteria (context) {
-      context.dispatch('resetOnsetCriteria')
-      context.dispatch('resetPatientCriteria')
-      context.dispatch('resetScanCriteria')
-    }
+    example
   }
 })
+
+export default store
